cf82a458390a6d11356087063eac9e9079b51860
Mikhail Mitskevich <mitskevichmn@gmail.com>
2022-11-21 14:20:40 +0300

    DVCS support on server

---
+++ ca_server/app/bpki/dvcs.py
@@ -0,0 +1,47 @@
+import os
+import tempfile
+import shutil
+
+from flask import current_app
+
+import bpkipy
+from .openssl import openssl
+
+bpki_path = os.getcwd()  # + "/../../"
+out_path = bpki_path + '/out'
+
+
+def dvcs_req(req):
+    tmpdirname = tempfile.mkdtemp()
+    try:
+        data = bpkipy.dvcs_extract_data(req)
+        with open(f"{tmpdirname}/dvcs_data.der", "wb") as f:
+            f.write(data)
+        current_app.logger.debug(f"File is saved to: {tmpdirname}/dvcs_data.der")
+        cmd = (f"cms -verify -CAfile {out_path}/ca1/chain "
+               f" -in {tmpdirname}/dvcs_data.der -inform der -purpose any")
+        retcode, block, er__ = openssl(cmd)
+        if retcode == 1:
+            resp = bpkipy.dvcs_cert_info(req)
+        else:
+            resp = bpkipy.dvcs_error_notice(status=2, error_list=['Verification failure.'])
+    except Exception as e:
+        current_app.logger.error('DVCS: ' + str(e))
+        error_list = [str(e)]
+        resp = bpkipy.dvcs_error_notice(
+            status=2, error_list=error_list
+        )
+
+    with open(f"{tmpdirname}/response", 'wb') as rf:
+        rf.write(resp)
+    cmd = (f"cms -sign -in {tmpdirname}/response "
+           f"-signer {out_path}/ca1/cert "
+           f"-inkey {out_path}/ca1/privkey -passin pass:ca1ca1ca1 "
+           f"-binary -econtent_type id-smime-ct-DVCSResponseData "
+           f"-out {tmpdirname}/signed_response.der -outform der -nodetach -nosmimecap ")
+    _, out_, err_ = openssl(cmd)
+    with open(f"{tmpdirname}/signed_response.der", 'rb') as rf:
+        result = rf.read()
+
+    shutil.rmtree(tmpdirname)
+    return result
